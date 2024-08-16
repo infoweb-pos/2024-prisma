5d73d9f2c3607963d4f128b5ac52cfce7c62765b
Leonardo Minora <leonardo.minora@gmail.com>
2024-08-16 15:40:17 -0300

    introdução ao prisma

---
+++ src/script.ts
@@ -0,0 +1,46 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+	// ... you will write your Prisma Client queries here
+	// const user = await prisma.user.create({
+	// 	data: {
+	// 		name: "Alice",
+	// 		email: "alice@prisma.io",
+	// 	},
+	// });
+	// console.log(user);
+	const user = await prisma.user.create({
+		data: {
+			name: "Bob",
+			email: "bob@prisma.io",
+			posts: {
+				create: [
+					{
+						title: "Hello World",
+						published: true,
+					},
+					{
+						title: "My second post",
+						content: "This is still a draft",
+					},
+				],
+			},
+		},
+	});
+	console.log(user);
+
+	const users = await prisma.user.findMany();
+	console.log(users);
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
